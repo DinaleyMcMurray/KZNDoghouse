8aea2e6d1e7470ed45eca2109036d97cf5eb38fd
Dinaley <131260562+DinaleyMcMurray@users.noreply.github.com>
2025-05-16 20:38:25 +0200

    LETS GET THIS +A

---
+++ TheDogHouse_Prototype/app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "vcmsa.projects.thedoghouse_prototype"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "vcmsa.projects.thedoghouse_prototype"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.room.common.jvm)
+    implementation(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.runtime.android)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("org.jetbrains:annotations:23.0.0")
+    }
+
+    exclude(group = "com.intellij", module = "annotations")
+}
---
+++ TheDogHouse_Prototype/app/src/main/java/vcmsa/projects/thedoghouse_prototype/MainActivity.kt
@@ -0,0 +1,32 @@
+package vcmsa.projects.thedoghouse_prototype
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.VideoView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+// Make sure you're importing the correct R
+import vcmsa.projects.thedoghouse_prototype.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val path = "android.resource://${packageName}/${R.raw.mainpage}"
+        videoView.setVideoURI(Uri.parse(path))
+        videoView.setOnPreparedListener { it.isLooping = true }
+        videoView.start()
+    }
+}
