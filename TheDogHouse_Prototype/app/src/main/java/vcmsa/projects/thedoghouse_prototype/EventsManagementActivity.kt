a2dc46cf9b3ff758389a7b7ad2c6417bf817a04a
Dinaley <131260562+DinaleyMcMurray@users.noreply.github.com>
2025-09-30 14:18:39 +0200

    Adding DogManagementActivity

---
+++ TheDogHouse_Prototype/app/src/main/java/vcmsa/projects/thedoghouse_prototype/EventsManagementActivity.kt
@@ -0,0 +1,19 @@
+package vcmsa.projects.thedoghouse_prototype
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EventsManagementActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_events_management)
+
+
+
+    }
+}
